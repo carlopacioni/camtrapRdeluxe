668892950f1f6bf4afaf5db534378ef19c311062
Carlo Pacioni <carl.pacioni@gmail.com>
2018-01-18 12:10:37 +1100

    Added folderIndices

---
+++ R/folderIndices.R
@@ -0,0 +1,6 @@
+folderIndices <- function(inDir) {
+  dirs <- list.dirs(inDir, full.names = TRUE,  recursive = FALSE)
+  tmp <- unlist(strsplit(dirs[[1]], split = "/", fixed = TRUE))
+  names(tmp) <- 1:length(tmp)
+  return(tmp)
+  }
