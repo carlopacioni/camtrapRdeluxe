d21ab413694ae18378307087329e05bb72a7e161
LukeDanielEmerson <lukedanielemerson@hotmail.com>
2019-01-08 16:15:04 +1100

    recordtable documentation ammendments

--- R/recordTable.R
+++ R/recordTable.R
@@ -76,8 +76,8 @@
 #'Phil Harvey to extract metadata from images. See
 #'\url{https://sno.phy.queensu.ca/~phil/exiftool/index.html} for exiftool
 #'download, and
-#'\url{https://cran.r-project.org/web/packages/camtrapR/vignettes/ImageOrganisation.html#exiftool}{Vignette
-#'1. Organising raw camera trap images in camtrapR} or
+#'\url{https://cran.r-project.org/web/packages/camtrapR/vignettes/ImageOrganisation.html#exiftool}
+#'{Vignette 1. Organising raw camera trap images in camtrapR}, or
 #'\code{\link{exiftoolPath}} for instructions on how to install exiftool for
 #'permanent or temporary access.
 #'
@@ -248,8 +248,8 @@
 #'  Custom image metadata must be organised hierarchically (tag group - tag;
 #'  e.g. "Species" - "Leopard Cat"). Detailed information on how to set up and
 #'  use metadata tags can be found in
-#'  \href{https://CRAN.R-project.org/package=camtrapR/vignettes/SpeciesIndividualIdentification.html#metadata-tagging}{Vignette
-#'   2: Identifying species and individuals in camtrapR}.
+#'  \href{https://CRAN.R-project.org/package=camtrapR/vignettes/SpeciesIndividualIdentification.html#metadata-tagging}
+#'  {Vignette 2: Identifying species and individuals in camtrapR}.
 #'
 #'  Custom image metadata tags must be written to the images. The function
 #'  cannot read tags from .xmp sidecar files. Make sure you set the preferences
@@ -259,8 +259,8 @@
 #'  Please note the section about defining argument \code{timeZone} in the
 #'  vignette on data extraction (accessible via
 #'  \code{vignette("DataExtraction")} or online
-#'  (\url{https://cran.r-project.org/package=camtrapR/vignettes/DataExtraction.html}{Vignette
-#'   3. Extracting Data from Camera Trapping Images})). }
+#'  (\url{https://cran.r-project.org/package=camtrapR/vignettes/DataExtraction.html}
+#'  {Vignette 3. Extracting Data from Camera Trapping Images})).
 #'
 #'@author Juergen Niedbella, Luke Emerson, Carlo Pacioni
 #'
